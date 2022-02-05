7696ce08b1141ac80b0a99e6b19dd771bb4c624b
yuliiaivanechko <yuliaivanechko@gmail.com>
2022-02-05 18:35:54 +0200

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Yuliia Ivanechko on 05.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
